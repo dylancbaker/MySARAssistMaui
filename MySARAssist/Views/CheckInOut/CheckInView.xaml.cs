fed86a678b92731f1a679341d1b2445a1976f480
DC Baker <dylan@greathat.ca>
2024-08-21 16:39:38 -0700

    generate qr

--- MySARAssist/Views/CheckInOut/CheckInView.xaml.cs
+++ MySARAssist/Views/CheckInOut/CheckInView.xaml.cs
@@ -1,9 +1,26 @@
+using MySARAssist.Services;
+
 namespace MySARAssist.Views.CheckInOut;
 
 public partial class CheckInView : ContentPage
 {
-	public CheckInView()
+    float _lastBrightness = 0.5f;
+
+    public CheckInView()
 	{
 		InitializeComponent();
-	}
+
+        // Example usage in your MAUI application
+        var brightnessService = new BrightnessService();
+        
+        brightnessService.SetBrightness(1.0f); // Set brightness to maximum
+
+
+        
+        // (Optional) Later on reset the brightness to what it was
+        //brightnessService.SetBrightness(previousBrightness);
+
+    }
+
+  
 }
