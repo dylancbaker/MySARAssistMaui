912b0810d969972629ce8030dae4df3d6e4d0eb4
DC Baker <dylan@greathat.ca>
2024-10-27 15:11:16 -0700

    Building out the incident items list

--- MySARAssist/ViewModels/IncidentInfoViewModels/IncidentInfoListViewModel.cs
+++ MySARAssist/ViewModels/IncidentInfoViewModels/IncidentInfoListViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MySARAssist.Services;
 using MySarAssistModels.IncidentItems;
 using System;
 using System.Collections.Generic;
@@ -10,14 +11,42 @@ namespace MySARAssist.ViewModels.IncidentInfoViewModels
 {
     public class IncidentInfoListViewModel : ObservableObject
     {
-        public List<IncidentItemType> AllIncidentItemTypes
-        {
-            get
-            {
-                return IncidentItemTools.GetItemTypes(true);
-            }
-        }
+        public List<IncidentItemType> AllIncidentItemTypes { get { return IncidentItemTools.GetItemTypes(true); } }
         public int FilterItemTypeId { get; set; } = 0;
+
+        private List<IncidentInfoItemViewModel>? _items;
+        public List<IncidentInfoItemViewModel>? items { get => _items; set => _items = value; }
+        public string? SearchText { get => _searchText; set { _searchText = value; } }
+
+        private string? _searchText;
+
+        public IncidentInfoListViewModel()
+        {
+
+        }
+
+        private async Task BuildItemList()
+        {
+            List<IncidentInfoItemViewModel> allItems = await new IncidentInfoService().GetAllIncidentInfoVMs();
+            //apply filters
+            if (FilterItemTypeId != 0)
+            {
+                IncidentItemType? filterType = IncidentItemTools.GetItemTypes(true).FirstOrDefault(o => o.Id == FilterItemTypeId);
+                if (filterType != null)
+                {
+                    allItems = allItems.Where(o => o.ItemType.Equals(filterType.Name, StringComparison.InvariantCultureIgnoreCase)).ToList();
+                }
+            }
+            //search text
+
+
+
+            items = allItems.OrderByDescending(o => o.DateCreated).ToList();
+            OnPropertyChanged(nameof(items));
+
+        }
+
 
     }
 }
+
