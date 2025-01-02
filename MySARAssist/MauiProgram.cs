da052cf452c562d28fa67a19bd1c89020d89f927
DC Baker <dylan@greathat.ca>
2025-01-01 18:24:43 -0800

    removed relic stuff

--- MySARAssist/MauiProgram.cs
+++ MySARAssist/MauiProgram.cs
@@ -10,7 +10,6 @@ using MySARAssist.Views;
 using MySARAssist.Views.CheckInOut;
 using MySARAssist.Views.RADeMS;
 using Microsoft.Maui.LifecycleEvents;
-using NewRelic.MAUI.Plugin;
 using MySARAssist.Models;
 
 namespace MySARAssist
@@ -78,46 +77,11 @@ namespace MySARAssist
             builder.Logging.AddDebug();
 #endif
 
-            builder.ConfigureLifecycleEvents(AppLifecycle => {
-#if ANDROID
-            AppLifecycle.AddAndroid(android => android
-               .OnCreate((activity, savedInstanceState) => StartNewRelic()));
-#endif
-#if IOS
-
-             AppLifecycle.AddiOS(iOS => iOS.WillFinishLaunching((_,__) => {
-                StartNewRelic();
-                return false;
-            }));
-#endif
-            });
 
             return builder.Build();
         }
 
-        private static void StartNewRelic()
-        {
-
-            CrossNewRelic.Current.HandleUncaughtException();
-
-            // Set optional agent configuration
-            // Options are: crashReportingEnabled, loggingEnabled, logLevel, collectorAddress, crashCollectorAddress, analyticsEventEnabled, networkErrorRequestEnabled, networkRequestEnabled, interactionTracingEnabled, webViewInstrumentation, fedRampEnabled, offlineStorageEnabled, newEventSystemEnabled, backgroundReportingEnabled
-            // AgentStartConfiguration agentConfig = new AgentStartConfiguration(crashReportingEnabled:false);
-
-
-            if (DeviceInfo.Current.Platform == DevicePlatform.Android)
-            {
-                CrossNewRelic.Current.Start("AA32d4cf0465ae3269a836f3f40801a1bd04f4e60e-NRMA");
-                // Start with optional agent configuration
-                // CrossNewRelic.Current.Start("<APP-TOKEN-HERE>", agentConfig);
-            }
-            else if (DeviceInfo.Current.Platform == DevicePlatform.iOS)
-            {
-                CrossNewRelic.Current.Start("AA6a713ecb46cfabf55e052bfd4f6297c55258ea9e-NRMA");
-                // Start with optional agent configuration
-                // CrossNewRelic.Current.Start("<APP-TOKEN-HERE>", agentConfig);
-            }
-        }
+       
     }
 
 
